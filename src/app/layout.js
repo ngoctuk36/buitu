b3f666edd14e966bd0a3e339c8b6fc5091c1ce1e
ngoctuk36 <bngoctu629@gmail.com>
2026-02-10 20:07:59 +0700

    update favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,12 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Tubui",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="vi">
